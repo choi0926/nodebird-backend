5ae9f2dac7b0c85271429d2925d757f9b2401cef
choihosung <ghtjd753753@gmail.com>
2020-04-22 15:53:07 +0900

    local passport의 Strategy설정

---
+++ passport/index.js
@@ -0,0 +1,20 @@
+import passport from 'passport';
+import db from '../models';
+import local from './local';
+
+module.exports = ()=>{
+    passport.serializeUser((user,done) =>{ //서버쪽에 id, cookie
+        return done(null, user.id);
+    });
+    passport.deserializeUser(async(id,done)=>{  // 요청 보낼때마다 실행되기때문에 "캐싱" 추가해야함. 
+        try{
+            const user = await db.User.findOne({where:{id}});
+            return done(null, user); //user 정보는 req.user에 저장됨
+        }catch(err){
+            console.error(err);
+            return done(err);
+        }
+    });
+   
+    local();
+}
---
+++ passport/local.js
@@ -0,0 +1,28 @@
+import passport from 'passport';
+import {Strategy} from 'passport-local';
+import bcrypt from 'bcrypt';
+import db from '../models';
+module.exports = () =>{
+    passport.use(new Strategy({
+        usernameField:'email', //req.body. 속성
+        passwordField:'password'
+    },async(email,password,done)=>{
+
+        try{
+            const user = await db.User.findOne({where:{email}});
+                if(!user){
+                    return done(null,false,{reaseon:'존재하지않는 사용자입니다.'});
+                }
+                const result =await bcrypt.compare(password,user.password);
+                if(result){
+                    return done(null,user);
+                }
+                return done(null,false,{reaseon:'비밀번호가 틀렸습니다.'});
+            
+        }catch(err){
+            console.error(err);
+            return done(err);
+        }
+    }
+    ))
+}
